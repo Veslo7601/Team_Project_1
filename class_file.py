51bab1419c685cf1e5517e8de210efb2a3bde2cd
Veslo7601 <igrok7601@gmail.com>
2024-02-16 19:45:01 +0200

    Basic version

---
+++ class_file.py
@@ -0,0 +1,145 @@
+"""Module providing a function """
+from collections import UserDict
+from datetime import datetime
+
+
+class Field:
+    """Class representing a default class"""
+
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    """Class representing a Name"""
+
+class Phone(Field):
+    """Class representing a Phone"""
+
+    @property
+    def value(self):
+        """Getter"""
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        """Setter"""
+        if len(str(value)) != 10:
+            raise Exception ("The number does not have 10 digits")
+        elif not value.isdigit():
+            raise Exception ("There are extra characters in the number")
+        else:
+            self.__value = value
+
+class Birthday(Field):
+    """Class representing a Birthday """
+
+    @property
+    def valid_birthday(self):
+        """Getter"""
+        return self.__value
+
+    @valid_birthday.setter
+    def valid_birthday(self,value):
+        """Setter"""
+        correct = datetime.strptime(value, '%d.%m.%Y')
+        if correct:
+            self.__value = correct
+        else:
+            raise ValueError ("The date is incorrect")
+
+class Record:
+    """Class representing a Record"""
+
+    def __init__(self, name, birthday=None):
+
+        self.name = Name(name)
+        self.phones = []
+        self.birthday = Birthday(birthday) if birthday else None
+
+    def add_phone(self,value):
+        """function for adding phones"""
+        self.phones.append(Phone(value))
+
+    def remove_phone(self,value):
+        """function for remove phones"""
+        if self.find_phone(value):
+            self.phones.remove(self.find_phone(value))
+        else:
+            raise ValueError()
+
+    def edit_phone(self,value,value_two):
+        """function for edit phones"""
+        if self.find_phone(value):
+            self.remove_phone(value)
+            self.add_phone(value_two)
+        else:
+            raise ValueError()
+
+    def find_phone(self,value):
+        """function for find phones"""
+        for phone in self.phones:
+            if str(phone) == str(value):
+                return phone
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+    def days_to_birthday(self):
+        """Function to find birthday"""
+        current_date = datetime.now().date()
+        day, month, year = str(self.birthday).split(".")
+        if int(month) >= current_date.month:
+            birthday = datetime(year=current_date.year, month=int(month), day=int(day)).date()
+        else:
+            birthday = datetime(year=current_date.year+1, month=int(month), day=int(day)).date()
+
+        days = birthday - current_date
+
+        if days.days < 0:
+            raise Exception("Birthday passed")
+        elif days.days == 0:
+            raise Exception("Birthday today")
+        else:
+            raise Exception(f"There are {days.days} days left until the birthday")
+
+class AddressBook(UserDict):
+    """Class representing a AddressBook"""
+
+    def add_record(self,Record):
+        """function for add record"""
+        return super().__setitem__(Record.name.value,Record)
+
+    def find(self,Name):
+        """function for find record"""
+        for key in self.data:
+            if str(key) == str(Name):
+                return super().__getitem__(key)
+        raise NameError("Contact does not exist, enter the name of an existing contact")
+
+    def delete(self,Name):
+        """function for delete record"""
+        return super().__delitem__(Name)
+
+    def iterator(self, value):
+        """Iterator"""
+        contact_list = []
+        for contact in self.data.values():
+            names = str(contact.name)
+            if names.find(value) != -1:
+                contact_list.append(f"{contact} have word {value}")
+
+            for phone in contact.phones:
+                contact_phone = str(phone)
+                if contact_phone.find(value) != -1:
+                    contact_list.append(f"{contact} have word {value}")
+
+        if len(contact_list) == 0:
+            return "No matches found"
+        else:
+            return contact_list
+
+#The file ends
---
+++ main.py
@@ -0,0 +1,148 @@
+"""Module providing a function """
+import pickle
+from collections import UserDict
+from datetime import datetime
+from class_file import AddressBook,Record
+
+def decorator(func):
+    """Decorator"""
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except KeyError:
+            return "Enter the correct command"
+        except ValueError:
+            return "Enter correct command"
+        except IndexError:
+            return "Enter the correct command, name and phone number"
+        except NameError as e:
+            return f"{e}"
+        except FileNotFoundError:
+            global book
+            book = AddressBook()
+            with open('Data.bin', 'wb') as file:
+                pickle.dump(book, file)
+                return "Create Data"
+        except Exception as e:
+            return f"{e}"
+    return wrapper
+
+@decorator
+def start():
+    """Load data"""
+    with open('Data.bin', 'rb') as file:
+        global book
+        book = pickle.load(file)
+    return "Bot start"
+
+def save():
+    """Save data"""
+    global book
+    with open('Data.bin', 'wb') as file:
+        pickle.dump(book, file)
+
+def command_hello():
+    """Function Hello"""
+    return "How can I help you?"
+
+def command_add_record(name,phone):
+    """Adding a contact to the Address Book"""
+    new_record = Record(name)
+    new_record.add_phone(phone)
+    book.add_record(new_record)
+    return "Contact added successfully"
+
+def command_find_record(value):
+    """Find a contact in the Address Book"""
+    return book.iterator(value)
+
+def command_delete_record(name):
+    """Deleting a contact in the Address Book"""
+    if book.find(name):
+        book.delete(name)
+        return f"Contact {name} deleted"
+
+def command_update_phone(name,phone):
+    """Adding a phone number"""
+    if book.find(name):
+        new_phone = book.find(name)
+        new_phone.add_phone(phone)
+        return new_phone
+
+def command_remove_phone(name, phone):
+    """Deleting a phone number"""
+    if book.find(name):
+        record = book.find(name)
+        record.remove_phone(phone)
+        return record
+
+def command_edit_phone(name,phone_one,phone_two):
+    """Changing the phone number"""
+    if book.find(name):
+        record = book.find(name)
+        record.edit_phone(phone_one,phone_two)
+        return record
+
+def command_show_all():
+    """Function show all phone number"""
+    for contact in book.values():
+        print(contact)
+
+def command_good_bye():
+    """Function close bot"""
+    global ACTIVE_BOT
+    ACTIVE_BOT = False
+    return "Good Bye!"
+
+def get_command(command):
+    """Function command bot"""
+    return command_list[command]
+
+command_list = {
+        "hello": command_hello,
+        "add" : command_add_record,
+        "find": command_find_record,
+        "delete": command_delete_record,
+
+        "update" : command_update_phone,
+        "remove": command_remove_phone,
+        "edit": command_edit_phone,
+
+        "show all": command_show_all,
+        "good bye": command_good_bye,
+        "close": command_good_bye,
+        "exit": command_good_bye,
+    }
+
+ACTIVE_BOT = False
+book = None
+
+@decorator
+def command_parser(user_input):
+    """Сommand parser"""
+    if user_input in ["show all", "hello", "good bye", "close", "exit"]:
+        return get_command(user_input)()
+    else:
+        user_input = user_input.split()
+        if user_input[0] in ["phone", "delete", "find"]:
+            return get_command(user_input[0])(user_input[1])
+        elif user_input[0] in ["remove", "update", "add"]:
+            return get_command(user_input[0])(user_input[1],(user_input[2]))
+        elif user_input[0] in ["edit"]:
+            return get_command(user_input[0])(user_input[1],(user_input[2]),(user_input[3]))
+        else:
+            raise ValueError()
+
+def main():
+    """Bot"""
+    print(start())
+    global ACTIVE_BOT
+    ACTIVE_BOT = True
+    while ACTIVE_BOT:
+        user_input = input("Enter the command: ").lower().strip()
+        print(command_parser(user_input))
+        save()
+
+main()
+#The file ends
