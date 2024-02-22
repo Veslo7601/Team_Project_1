8229749eaa6bc37ac5e44959b445567e27d5021d
Veslo7601 <igrok7601@gmail.com>
2024-02-22 09:47:07 +0200

    modified:   personal_bot_assistant/personal_bot_assistant/___init___.py 	renamed:    personal_bot_assistant/personal_bot_assistant/bot.py -> personal_bot_assistant/personal_bot_assistant/personal_bot.py 	modified:   personal_bot_assistant/setup.py

--- personal_bot_assistant/personal_bot_assistant/___init___.py
+++ personal_bot_assistant/personal_bot_assistant/___init___.py
@@ -1,3 +1,3 @@
-from .bot import main
+from .personal_bot import main
 
 __all__ = ['main']
--- personal_bot_assistant/setup.py
+++ personal_bot_assistant/setup.py
@@ -4,7 +4,7 @@ setup(
     name="personal_bot_assistant",
     version="1.1.4",
     entry_points={
-        "console_scripts":["bot-start=personal_bot_assistant.bot:main"]
+        "console_scripts":["bot-start=personal_bot_assistant.personal_bot:main"]
     },
     packages=find_packages()
 )
