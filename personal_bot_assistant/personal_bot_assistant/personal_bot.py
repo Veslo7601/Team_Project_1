e5ccc7df6c432876bbb7c5db029a5ebb8eb9a762
Veslo7601 <igrok7601@gmail.com>
2024-02-22 21:00:32 +0200

    deleted:    Data.bin 	modified:   personal_bot_assistant/personal_bot_assistant/personal_bot.py 	renamed:    readme.md -> personal_bot_assistant/readme.md 	new file:   requirements.txt

--- personal_bot_assistant/personal_bot_assistant/personal_bot.py
+++ personal_bot_assistant/personal_bot_assistant/personal_bot.py
@@ -1,11 +1,8 @@
-
 """Module providing a function """
 import pickle
-import os
-import shutil
 from collections import UserDict
 from datetime import datetime
-from class_file import AddressBook,Record
+from .class_file import AddressBook, Record
 
 def decorator(func):
     """Decorator"""
@@ -37,7 +34,7 @@ def start():
     with open('Data.bin', 'rb') as file:
         global book
         book = pickle.load(file)
-    return "Bot started"
+    return "Bot start"
 
 def save():
     """Save data"""
@@ -150,110 +147,9 @@ def command_add_birthday(name, birthday):
         new_birthday = book.find(name)
         new_birthday.add_birthday(birthday)
         return "Birthday added successfully"
-    
-def command_show_birthday(days: int):
-    cnt = 0
-    for contact in book.values():
-        if contact.days_to_birthday() - int(days) == 0:
-            print(f'{contact}')
-            cnt += 1
-    if cnt > 0:
-        return f"Contacts that have birthday in {days} days are printed above"
-    else:
-        return f"There are no contacts that have birthdays in {days} days in the AddressBook"
-        
-
-def command_help_info():
-    return """Commands list:\n
-        *hello - prints greeting \n
-        *add <contact name> <phone number>- adds record if contact name is not present, adds phone if contact name is present and phone number differs from other \n
-        *update <contact name> <old phone> <new phone>- changes contact phone by name \n
-        *delete <contact name>- delete contact or delete \n
-        *remove <contact name> <phone> - delete specified phone for the contact \n        
-        *show all - prints entire Address Book \n
-        *find <name or phone> - filter by name letters or phone number \n
-        *exit, good bye, close - saves changes to database and exit \n
-        *add-address <name, address> - adds address for the contact with specified name \n
-        *add-email <name, email> - adds email for the specified contact \n
-        *add-birthday <name, borthday> - adds birthday for the specified contact \n
-        *write <name, note> - adds note for the contact with specified name \n
-        *delete-note <name, note> - removes note for the contact with specified name \n
-        *edit-note <name, note> - edit note \n
-        *find-note <note> - find contact with specified note \n
-        *show-birthdate <number of days> - shows all contacts wich have birthday on a date that will occure in specified number of days \n
-        *sort <folder path> - sorts the files in the specified folder\n
-        *add-tag <name, tag> - adds a tag to the specified contact\n
-        *find-tag <tag> - searches for contacts by the specified tags\n"""
-
-def command_remove_address(name, address):
-    """Deleting a address"""
-    if book.find(name):
-        record = book.find(name)
-        record.remove_address(address)
-        return 'Address deleting'
-
-def command_remove_email(name, email):
-    """Deleting a email"""
-    if book.find(name):
-        record = book.find(name)
-        record.remove_email(email)
-        return 'Email deleting'
-
-def command_remove_birthday(name):
-    """Deleting a birthday"""
-    if book.find(name):
-        record = book.find(name)
-        record.remove_birthday()
-        return 'Birthda deleting'
-
-def sort_files_by_category(folder_path):
-    """Function sort files by category"""
-    if not os.path.exists(folder_path):
-        return "The path does not exist."
-
-    categories = {
-        'image': ['.jpg', '.jpeg', '.png', '.gif'],
-        'video': ['.mp4', '.avi', '.mkv'],
-        'documents': ['.pdf', '.doc', '.docx', '.txt'],
-        'other': []  
-    }
-
-    for file_name in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, file_name)
-        if os.path.isfile(file_path):
-            _, file_extension = os.path.splitext(file_name)
-            found_category = False
-            for category, extensions in categories.items():
-                if file_extension.lower() in extensions:
-                    found_category = True
-                    category_folder = os.path.join(folder_path, category)
-                    if not os.path.exists(category_folder):
-                        os.makedirs(category_folder)
-                    shutil.move(file_path, os.path.join(category_folder, file_name))
-                    break
-            if not found_category:
-                other_folder = os.path.join(folder_path, 'інше')
-                if not os.path.exists(other_folder):
-                    os.makedirs(other_folder)
-                shutil.move(file_path, os.path.join(other_folder, file_name))
-
-    return "Sorting is complete!"
-
-def command_add_teg(name, tags):
-    """Adding a tags"""
-    if book.find(name):
-        new_tags = book.find(name)
-        new_tags.add_teg(tags)
-        return "Tags added successfully"
-    
-def command_find_teg(value):
-    """Function find teg"""
-    for i in book.search_notes_by_tag(value):
-        print(i)
 
 command_list = {
         "hello": command_hello,
-        "help": command_help_info,
         "add": command_add_record,
         "find": command_find_record,
         "delete": command_delete_record,
@@ -264,7 +160,6 @@ command_list = {
 
         "show all": command_show_all,
         "good bye": command_good_bye,
-        "show-birthdate": command_show_birthday,
         "close": command_good_bye,
         "exit": command_good_bye,
 
@@ -272,18 +167,10 @@ command_list = {
         "add-email": command_add_email,
         "add-birthday": command_add_birthday,
 
-        "remove-address": command_remove_address,
-        "remove-email": command_remove_email,
-        "remove-birthday": command_remove_birthday,
-
         "write": command_add_note,
         "delete-note": command_delete_note,
         "edit-note": command_edit_note,
         "find-note": command_find_note,
-
-        "sort": sort_files_by_category,
-        "add-tag": command_add_teg,
-        "find-tag": command_find_teg,
     }
 
 ACTIVE_BOT = False
@@ -292,16 +179,15 @@ book = None
 @decorator
 def command_parser(user_input):
     """Сommand parser"""
-    if user_input in ["show all", "hello", "good bye", "close", "exit", "help"]:
+    if user_input in ["show all", "hello", "good bye", "close", "exit"]:
         return get_command(user_input)()
     else:
         user_input = user_input.split()
-        if user_input[0] in ["phone", "delete", "find", "delete-note", "find-note", "show-birthdate", "remove-birthday", "sort","find-tag",]:
+        if user_input[0] in ["phone", "delete", "find", "delete-note", "find-note",]:
             return get_command(user_input[0])(user_input[1])
-        elif user_input[0] in ["remove", "update", "add", "add-email", "add-birthday", "remove-address", "remove-email", "add-tag"]:
+        elif user_input[0] in ["remove", "update", "add", "add-email", "add-birthday"]:
             return get_command(user_input[0])(user_input[1],(user_input[2]))
         elif user_input[0] in ["write","add-address", "edit-note"]:
-
             return get_command(user_input[0])(user_input[1],(user_input[2:]))
         elif user_input[0] in ["edit"]:
             return get_command(user_input[0])(user_input[1],(user_input[2]),(user_input[3]))
@@ -316,11 +202,7 @@ def main():
     while ACTIVE_BOT:
         user_input = input("Enter the command: ").lower().strip()
         print(command_parser(user_input))
-        print("____________________________________")
         save()
 
-
-if __name__ == '__main__':
-    main()
-
+main()
 #The file ends
